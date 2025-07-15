577d3e617439103149e8eafd372b5d23a82f2da2
ehddnr301 <dy950328@gmail.com>
2025-07-15 19:49:44 +0900

    temp

--- sample_dag.py
+++ sample_dag.py
@@ -1,35 +1,30 @@
-from datetime import datetime, timedelta
-
+import pendulum
+from datetime import timedelta
 from airflow import DAG
-
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
-
-from kubernetes.client import models as k8s
-
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
-    'timezone': 'Asia/Seoul',
 }
 
 with DAG(
     dag_id='example_kubernetes_pod_airflow_3_0_2',
     default_args=default_args,
-    start_date=datetime(2025, 7, 10),
-    schedule='0 3 * * *',  # KST 03:00 (새벽 3시)
+    # pendulum을 이용해 tz-aware datetime 지정
+    start_date=pendulum.datetime(2025, 7, 10, tz='Asia/Seoul'),
+    schedule='0 3 * * *',  # 매일 KST 03:00 실행
     catchup=False,
     tags=['example'],
 ) as dag:
 
-    # GH Archive 데이터 수집 태스크
     collect_gh_archive = KubernetesPodOperator(
         task_id='collect_gh_archive',
         name='gh-archive-collector',
         namespace='default',
-        image='ehddnr/gh-archive-collector:latest',  # Docker 이미지 사용
+        image='ehddnr/gh-archive-collector:latest',
         image_pull_policy='Always',
         cmds=['python'],
         arguments=['/app/gh_archive_daily_collector.py', '{{ ds }}', 'CausalInferenceLab'],
@@ -42,5 +37,3 @@ with DAG(
         termination_grace_period=30,
         reattach_on_restart=False,
     )
-
-    collect_gh_archive
