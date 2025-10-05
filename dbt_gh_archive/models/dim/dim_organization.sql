331b9108b65a1b87da9c138c1fcb67861011bc85
ehddnr301 <dy950328@gmail.com>
2025-10-05 13:54:44 +0900

    조직 및 레포 DIM에 최초 등장일(first_seen_date) 추가: 각 조직과 레포의 최초 관측 날짜를 포함하도록 SQL 쿼리 수정 및 관련 설명 업데이트

--- dbt_gh_archive/models/dim/dim_organization.sql
+++ dbt_gh_archive/models/dim/dim_organization.sql
@@ -1,8 +1,15 @@
 {{ config(materialized='table') }}
 
--- 조직 필터용 DIM: 단일 컬럼
-select distinct
-  organization
-from {{ ref('dw_activity_daily') }}
-where organization is not null
+-- 조직 DIM: 조직별 최초 등장일(first_seen_date) 포함
+with base as (
+  select organization, min(base_date) as first_seen_date
+  from {{ ref('dw_activity_daily') }}
+  where organization is not null
+  group by 1
+)
+
+select
+  b.organization,
+  b.first_seen_date
+from base b
 
--- dbt_gh_archive/models/dim/dim_repository.sql
+++ dbt_gh_archive/models/dim/dim_repository.sql
@@ -1,12 +1,22 @@
 {{ config(materialized='table') }}
 
--- 레포 필터용 DIM: organization 포함
-select distinct
-  organization,
-  case
-    when repo_name is not null and strpos(repo_name, '/') > 0 then split_part(repo_name, '/', 2)
-    else repo_name
-  end as repo
-from {{ ref('dw_activity_daily') }}
-where repo_name is not null and organization is not null
+-- 레포 DIM: organization 포함 + 레포 최초 등장일(first_seen_date)
+with base as (
+  select
+    organization,
+    case
+      when repo_name is not null and strpos(repo_name, '/') > 0 then split_part(repo_name, '/', 2)
+      else repo_name
+    end as repo,
+    min(base_date) as first_seen_date
+  from {{ ref('dw_activity_daily') }}
+  where repo_name is not null and organization is not null
+  group by 1, 2
+)
+
+select
+  b.organization,
+  b.repo,
+  b.first_seen_date
+from base b
 
